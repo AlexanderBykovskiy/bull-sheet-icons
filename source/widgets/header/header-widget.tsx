da269f338a8b23a659da48396990580c68d2d46c
Alexander <bykovskiyan@gmail.com>
2024-10-15 22:24:31 +0200

    feat: add sass

---
+++ source/widgets/header/header-widget.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import classes from './styles.module.sass'
+import {Logo} from "@shared/ui/logo/logo";
+
+export const HeaderWidget: React.FC = () => {
+    return (
+        <header className={classes.header}>
+            <div className={classes.logo}>
+                <Logo />
+            </div>
+
+        </header>
+    )
+}
